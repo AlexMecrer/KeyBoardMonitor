ba77921a910d354a65aeee9291137c8bcc223099
墨染 <2927518585@qq.com>
2018-05-15 18:52:51 +0800

    Create KeyBoardMonitor.h
    We Need This Header File

---
+++ KeyBoardMonitor.h
@@ -0,0 +1,52 @@
+#include<ntddkbd.h>
+
+
+
+#define DEVNAME L"\\Device\\AlexMecrer"
+#define SYMNAME L"\\??\\jiuli"
+#define KEYBOARD L"\\Driver\\Kbdclass"
+
+
+typedef struct {
+	PDEVICE_OBJECT OrgnDev;
+	PDEVICE_OBJECT FiltDev;
+	PDEVICE_OBJECT AttchDev;
+}MYEXT,*PMEXT;
+
+
+extern POBJECT_TYPE *IoDriverObjectType;
+
+
+
+NTSTATUS
+ObReferenceObjectByName(
+	__in PUNICODE_STRING ObjectName,
+	__in ULONG Attributes,
+	__in_opt PACCESS_STATE AccessState,
+	__in_opt ACCESS_MASK DesiredAccess,
+	__in POBJECT_TYPE ObjectType,
+	__in KPROCESSOR_MODE AccessMode,
+	__inout_opt PVOID ParseContext,
+	__out PVOID *Object
+);
+
+NTSTATUS AttchAllDevice(PDRIVER_OBJECT Driver);
+
+NTSTATUS KeyBoardDispath(
+	IN PDEVICE_OBJECT DeviceObject,
+	IN PIRP Irp
+	);
+
+NTSTATUS KeyBoardDispathRead(
+	IN PDEVICE_OBJECT DeviceObject,
+	IN PIRP Irp
+);
+
+VOID KeyBoardUnLoad(PDRIVER_OBJECT DriverObject);
+
+
+NTSTATUS IoCompletionRoutine(
+	PDEVICE_OBJECT DeviceObject,
+	PIRP Irp,
+	PVOID Context
+);
